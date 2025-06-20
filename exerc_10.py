3b6811bfdfb06572a70dbb89e72e74b04cc07ceb
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-20 13:46:01 -0300

    Update exerc_10.py

--- exerc_10.py
+++ exerc_10.py
@@ -1,5 +1,6 @@
 frase = str(input('Digite uma frase: ')).upper()
 print('A letra A aparece {}.'.format(frase.count('A')))
 print('A letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A ultima letra a apareceu na posição {}'.format(frase.rfind('A')+1))
 
 
